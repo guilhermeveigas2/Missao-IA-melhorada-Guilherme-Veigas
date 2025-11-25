7bbd70c17cfa91d8ac6a335421f6672cd38080ae
guilhermeveigas2 <146458725+guilhermeveigas2@users.noreply.github.com>
2025-11-25 11:36:52 -0300

    09

--- aleatorio.js
+++ aleatorio.js
@@ -1,5 +1,4 @@
-
-function aleatorio(lista){
-    const posicao = Math.floor(Math.random()*lista.length);
-    return lista[posicao];
+export function aleatorio(lista){
+const posicao = Math.floor(Math.random()*lista.length);
+return lista[posicao];
 }
